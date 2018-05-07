256932e6d3c40c9da3f8bcf8acc1b623ea608f83
AK7 <abdelrhman.khater111@gmail.com>
2018-05-07 14:08:55 +0200

    add Glow_Hockey

---
+++ GlowHockey/Ball.py
@@ -0,0 +1,10 @@
+class Circle:
+    def __init__(self, ri, re, x, y, x_prev, y_prev, s, e):
+        self.ri = ri
+        self.re = re
+        self.x = x
+        self.y = y
+        self.s = s
+        self.e = e
+        self.x_prev = x_prev
+        self.y_prev = y_prev
---
+++ GlowHockey/Wall.py
@@ -0,0 +1,7 @@
+class Wall:
+    def __init__(self, left, right, bottom, top):
+        self.left = left
+        self.right = right
+        self.top = top
+        self.bottom = bottom
+
